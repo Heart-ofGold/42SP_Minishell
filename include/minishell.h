f1718fdd35653c88165f356dbe39d3a8ddaaef60
Fernanda Alves fahl <fe.alves.fahl@gmail.com>
2023-03-09 20:42:25 -0300

    Libft working, pls

---
+++ include/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feralves <feralves@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 20:11:08 by feralves          #+#    #+#             */
+/*   Updated: 2023/03/09 20:42:01 by feralves         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+// Functions
+
+#endif
