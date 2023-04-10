cdf6a17c2da2ff42e691f846ca535de0d4757919
Milena <milena.c@aluno.ifsp.edu.br>
2023-04-10 14:26:36 -0300

    fix CI

--- sources/pearl/exit.c
+++ sources/pearl/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: feralves <feralves@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: mcarecho <mcarecho@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 20:30:15 by feralves          #+#    #+#             */
-/*   Updated: 2023/04/06 22:27:26 by feralves         ###   ########.fr       */
+/*   Updated: 2023/04/10 14:25:52 by mcarecho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ void	ft_free_all(t_token *tokens)
 
 	if (tokens)
 	{
-		while (tokens->next_cmd)
+		while (tokens->next_token)
 		{
 			aux = tokens;
 			ft_free_array(tokens->cmd);
 			free(tokens->value);
 			free(tokens->path);
-			tokens = tokens->next_cmd;
+			tokens = tokens->next_token;
 			free(aux);
 		}
 		free(tokens->path);
