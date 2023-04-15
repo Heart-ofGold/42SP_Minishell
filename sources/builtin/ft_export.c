b97973a4902dbc8a4bde4edcada2dbe1e0e00f36
ferfahl <fe.alves.fahl@gmail.com>
2023-04-14 21:00:48 -0300

    [builtin] export fixed, needs to be cleaned

--- sources/builtin/ft_export.c
+++ sources/builtin/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: feralves <feralves@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 19:00:42 by feralves          #+#    #+#             */
-/*   Updated: 2023/04/14 20:55:03 by feralves         ###   ########.fr       */
+/*   Updated: 2023/04/14 20:59:55 by feralves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,14 @@ int	check_valid_var(char *name)
 void	append_env(t_mini_env **mini_env, char *name, char *value)
 {
 	t_mini_env	**aux;
+	t_mini_env	*temp;
 
 	aux = mini_env;
+	temp = (*mini_env)->next;
 	(*aux)->next = malloc(sizeof(t_mini_env));
 	(*aux)->next->name = ft_strdup(name);
 	(*aux)->next->value = ft_strdup(value);
-	(*aux)->next->next = NULL;
+	(*aux)->next->next = temp;
 }
 
 void	ft_change_value(t_mini_env **mini_env, char *cmd)
@@ -61,9 +63,9 @@ void	ft_export(t_token *token, t_mini_env *mini_env)
 	while (token->cmd[++i])
 	{
 		command = ft_split(token->cmd[i], '=');
-		if (!command[1] || !check_valid_var(token->cmd[0]))
+		if (!command[1] || !check_valid_var(command[0]))
 			return ;
-		while (aux->next)
+		while (aux->next->next)
 		{
 			if (!ft_strncmp(command[0], aux->name, ft_strlen(command[0])))
 				return (ft_change_value(&aux, command[1]));
